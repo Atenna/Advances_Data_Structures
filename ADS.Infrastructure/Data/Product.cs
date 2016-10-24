230e2b0e8d02676325e11a79fa38fbfddda4164e
Ivana Lieska <idrabikova@gmail.com>
2016-10-24 18:37:26 +0200

    new classes

---
+++ ADS.Infrastructure/Data/Product.cs
@@ -0,0 +1,26 @@
+﻿using ADS.ADS.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADS.ADS.Data
+{
+    public class Product : IData<Product>
+    {
+        public string Name { get; set; }
+        public string DateLatestUsage { get; set; }
+        public DateTime DateStartExport { get; set; }
+        public DateTime DateExpectedEndExport { get; set; }
+        public string ProductId { get; set; }
+        public int CarId { get; set; }
+        public string OriginStockId { get; set; }
+        public string TargetStockId { get; set; }
+
+        public int CompareTo(Product other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ADS.Infrastructure/Data/Stock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADS.ADS.DataStructures;
+
+namespace ADS.ADS.Data
+{
+    public class Stock : IData<Stock>
+    {
+        public string StockId { get; set; }
+        public BinarySearchTree<Product> Products;
+
+        public Stock()
+        {
+            Products =  new BinarySearchTree<Product>();
+            StockId = "blabla";
+        }
+
+        public int CompareTo(Stock other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
