c246a585f453c18118b243681eea3b5b59329c19
Ivana Lieska <idrabikova@gmail.com>
2016-10-25 23:11:46 +0200

    new classes added

---
+++ ADS.Infrastructure/Data/Library/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ADS.Services;
+
+namespace ADS.ADS.Data.Library
+{
+    public enum Genres
+    {
+        
+    }
+
+    public class Book : IData<Book>
+    {
+        public string Author;
+        public string Title;
+        public string CodeIsbn;
+        public string CodeEan;
+        public Genres[] GenreList;
+        public Time TimeStandardBorrow;
+        public Time TimeOfBorrow;
+        public Time TimeToReturn;
+        public int UniqueId;
+        public int Fee;
+
+        public Book()
+        {
+            
+        }
+
+        public int CompareTo(Book other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ADS.Infrastructure/Data/Library/Reader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADS.ADS.DataStructures;
+
+namespace ADS.ADS.Data.Library
+{
+    public class Reader : IData<Reader>
+    {
+        public string Name;
+        public String Surname;
+        public int UniqueId;
+        public AvlTree<Book> BooksBorrowedInPast;
+        public AvlTree<Book> BooksCurrentlyBorrowed;
+        public AvlTree<Book> LateBookReturns;
+
+        public Reader()
+        {
+            
+        }
+
+        public int CompareTo(Reader other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
