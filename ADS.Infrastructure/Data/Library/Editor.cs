96a8da2bec6fcadf6feb179b9bb0246ab525c826
Ivana Lieska <idrabikova@gmail.com>
2016-11-02 11:12:27 +0100

    Isbn generator

---
+++ ADS.Infrastructure/Data/Library/Editor.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ADS.ADS.Data.Library
+{
+    public class Editor
+    {
+        public class IsbnComparer : IComparer<Editor>
+        {
+            public int Compare(Editor x, Editor y)
+            {
+                if (x.EditorCode > y.EditorCode)
+                {
+                    return 1;
+                }
+                if (x.EditorCode < y.EditorCode)
+                {
+                    return -1;
+                }
+                return 0;
+            }
+        }
+
+        public int EditorCode { get; }
+        public string EditorName { get; }
+        public int GroupCode { get; }
+        public int Published { get; private set; }
+
+        public Editor(int code, int group, string name)
+        {
+            EditorCode = code;
+            GroupCode = group;
+            EditorName = name;
+            Published = 1;
+        }
+
+        public string GenerateIsbn()
+        {
+            string code = "978-" + GroupCode + "-" + EditorCode + "-" + PublishedWithZeros() + CheckSum();
+            Published++;
+            return code;
+        }
+
+        private string PublishedWithZeros()
+        {
+            int length = 3 + GroupCode.ToString().Length + EditorCode.ToString().Length + Published.ToString().Length;
+            string publishedWithZeros = "";
+            for (int i = 0; i < 12-length; i++)
+            {
+                publishedWithZeros += "0";
+            }
+            return publishedWithZeros + Published;
+        }
+
+        private int CheckSum()
+        {
+            int checksum = 0;
+            int value = 1;
+            string groupCode = GroupCode.ToString();
+            foreach (var letter in groupCode)
+            {
+                var number = int.Parse(letter.ToString());
+                checksum += number*value;
+                value = value == 1 ? 3 : 1;
+            }
+            string editorCode = EditorCode.ToString();
+            foreach (var letter in editorCode)
+            {
+                var number = int.Parse(letter.ToString());
+                checksum += number * value;
+                value = value == 1 ? 3 : 1;
+            }
+            string published = Published.ToString();
+            foreach (var letter in editorCode)
+            {
+                var number = int.Parse(letter.ToString());
+                checksum += number * value;
+                value = value == 1 ? 3 : 1;
+            }
+            return checksum = 10 - (checksum%10);
+        }
+    }
+}
---
+++ ADS.Services/DataGenerating/UniqueKeyGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ADS.Services.DataGenerating
+{
+    public class UniqueKeyGenerator
+    {
+        private int _group = 0;
+        private int _editor = 0;
+        private Random rnd = new Random();
+
+        public int GenerateEditorCode()
+        {
+            _editor = rnd.Next(1000000) + 10;
+            return _editor;
+        }
+
+        public int GenerateGroupCode()
+        {
+            _group = rnd.Next(100);
+            return _group;
+        }
+    }
+}
