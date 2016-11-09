a76e765463947fd4dfbe344d7b9b5e8d074053a9
Ivana Lieska <idrabikova@gmail.com>
2016-11-10 00:27:41 +0100

    comparators

--- ADS.Infrastructure/Data/Library/Reader.cs
+++ ADS.Infrastructure/Data/Library/Reader.cs
@@ -1,10 +1,39 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using ADS.ADS.DataStructures;
 
 namespace ADS.ADS.Data.Library
 {
     public class Reader
     {
+        public class ReaderIdComparator : IComparer<Reader>
+        {
+            public int Compare(Reader x, Reader y)
+            {
+                long xd = x.UniqueId;
+                long yd = y.UniqueId;
+                if (xd > yd)
+                {
+                    return 1;
+                }
+                else if (xd < yd)
+                {
+                    return -1;
+                }
+                return 0;
+            }
+        }
+        public class ReaderNameComparator : IComparer<Reader>
+        {
+            public int Compare(Reader x, Reader y)
+            {
+                string xd = x.Surname+x.Name;
+                string yd = y.Surname + y.Name;
+                return string.Compare(xd, yd);
+            }
+        }
+
         public string Name;
         public String Surname;
         public int UniqueId;
@@ -20,5 +49,10 @@ namespace ADS.ADS.Data.Library
             HasBlockedBorrowing = false;
         }
 
+
+        public override string ToString()
+        {
+            return Name + " " + Surname + ", " + UniqueId;
+        }
     }
 }
