eda6addc4fe4109c1cdb3756d3900d420957a818
Ivana Lieska <idrabikova@gmail.com>
2016-11-10 00:28:37 +0100

    another data structure :athletic_shoe:

---
+++ ADS.InfrastructureTests/DataStructures/SinglyLinkedListTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ADS.ADS.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADS.ADS.DataStructures.Tests
+{
+    [TestClass()]
+    public class SinglyLinkedListTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            SinglyLinkedList<int> zoznam = new SinglyLinkedList<int>();
+
+            zoznam.Add(2);
+            zoznam.Add(3);
+            zoznam.Add(9);
+            zoznam.Add(1);
+
+            int ocakavana;
+            ocakavana = 3;
+
+            int realna = zoznam.First.Next.Data;
+
+            Assert.AreEqual(ocakavana, realna);
+            
+
+        }
+    }
+}
