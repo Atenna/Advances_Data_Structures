1c87cf774e9aa5980d5424c75928be98b5339844
Ivana Lieska <idrabikova@gmail.com>
2016-11-24 16:25:01 +0100

    Borrowing itself :zap:

--- ADS.Infrastructure/Data/Library/Borrowing.cs
+++ ADS.Infrastructure/Data/Library/Borrowing.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace ADS.ADS.Data.Library
 {
-    class Borrowing
+    public class Borrowing
     {
+        public class BorrowingComparator : IComparer<Borrowing>
+        {
+            public int Compare(Borrowing x, Borrowing y)
+            {
+                //return string.Compare(x.Title, y.Title);
+                if (x.DateOfBorrow > y.DateOfBorrow)
+                {
+                    return 1;
+                }
+                if (x.DateOfBorrow < y.DateOfBorrow)
+                {
+                    return -1;
+                }
+                return 0;
+            }
+        }
+
+        public Borrowing(Book b, Reader r)
+        {
+            Reader = r;
+            Book = b;
+            if (b.TimeOfBorrow != null) DateOfBorrow = b.TimeOfBorrow.Value;
+            if (b.TimeOfReturn != null) DateOfReturn = b.TimeOfReturn.Value;
+        }
         public Reader Reader { get; }
         public Book Book { get; set; }
 
@@ -17,5 +41,11 @@ namespace ADS.ADS.Data.Library
         public int Fee { get; set; }
         public bool FeeToPay { get; set; }
         public bool FeePaid { get; set; }
+
+        public override string ToString()
+        {
+            var fee = FeeToPay ? Fee+"" : "None";
+            return Book.ToString() + "/n/t Fee:" + fee;
+        }
     }
 }
